beb85e8b5eaf88c2ddb22895adb3a3e4c54933fb
Pranav <bean.pranav@gmail.com>
2015-09-10 21:06:03 -0400

    player array type fix

--- db/migrate/20150911005003_create_players.rb
+++ db/migrate/20150911005003_create_players.rb
@@ -4,8 +4,8 @@ class CreatePlayers < ActiveRecord::Migration
       t.integer :user_id
       t.integer :game_id
       t.integer :salary_total
-      t.integer :skill_level, :array => true, :length => 4, :default => '{0,0,0,0}'
-      t.integer :skill_total_points, :array => true, :length => 4, :default => '{0,0,0,0}'
+      t.integer :skill_level, :array => true, :length => 4
+      t.integer :skill_total_points, :array => true, :length => 4
 
       t.timestamps
     end
