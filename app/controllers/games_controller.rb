9431347396c314d4239664a8e89f31eec60dbbbc
Pranav <bean.pranav@gmail.com>
2016-02-15 16:48:08 -0500

    debug

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy, :continue_game, :complete_game, :generate_forms]
   before_action :authenticate_user!
   before_action :authenticate_admin, only: [:index]
-  before_action :correct_user, except: [:index, :new, :create]
+  before_action :correct_user, only: [:show, :edit, :update]
   include GamesHelper
 
   def index
